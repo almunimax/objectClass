c894e62707914fea26c499b041173aa57c36197c
almunimax <almunimax@mail.ru>
2022-12-24 16:46:31 +0300

    objectClass

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ public class Main {
         Book nos = new Book("Нос", gogol,1989);
         Book voina = new Book("Война и мир", tolstoy,2022);
         System.out.println(nos);
+        nos.setPublished(1900);
+        System.out.println(nos);
+
 
     }
 
