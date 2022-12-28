2a871d86457ef5937fca77eb80179883ca956236
almunimax <almunimax@mail.ru>
2022-12-28 15:45:42 +0300

    1

--- src/objectClass/Author.java
+++ src/objectClass/Author.java
@@ -15,33 +15,9 @@ public class Author {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
-    @Override
-    public String toString() {
-        return name + " " + lastName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Author author = (Author) o;
-        return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, lastName);
-    }
 }
--- src/objectClass/Book.java
+++ src/objectClass/Book.java
@@ -17,10 +17,6 @@ public class Book {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public Author getAuthor() {
         return author;
     }
@@ -33,21 +29,5 @@ public class Book {
         this.published = published;
     }
 
-    @Override
-    public String toString() {
-        return "Название книги: " + title + ", автор " + author + ", год издания " + published;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author);
-    }
 }
